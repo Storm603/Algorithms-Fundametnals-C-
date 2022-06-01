a7b4a0462610e08c7232d4f788b41461a4f29542
Kristiyan Stoyanov <93555141+Storm603@users.noreply.github.com>
2022-06-01 23:56:07 +0300

    Add files via upload

---
+++ T02SelectionSort/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace T02SelectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Console.WriteLine(string.Join(" ", SelectionSort(nums)));
+            
+
+        }
+
+        private static int[] SelectionSort(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                bool toSwap = false;
+                int smallest = 0;
+                int index = 0;
+                for (int j = 1 + i; j < nums.Length; j++)
+                {
+                    if (nums[j] < nums[i])
+                    {
+                        smallest = nums[j];
+                        index = j;
+                        toSwap = true;
+                    }
+                }
+
+                if (toSwap)
+                {
+                    nums = Swap(nums, smallest, index, i);
+                }
+            }
+
+            return nums;
+        }
+
+        private static int[] Swap(int[] nums, int smallest, int index, int i)
+        {
+            int temp = nums[i];
+            nums[i] = smallest;
+            nums[index] = temp;
+
+            return nums;
+        }
+    }
+}
